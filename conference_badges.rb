52f8ce17934f2061776ae79e14bb69ec48b81dd7
Samuel Cornet <samuel@noukod.com>
2018-09-19 16:31:55 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -27,6 +27,6 @@ def assign_rooms(conference_speakers)
 end
 
 def printer(conference_speakers)
- return batch_badge_creator(conference_speakers)
- return assign_rooms(conference_speakers)
+ p batch_badge_creator(conference_speakers)
+ p assign_rooms(conference_speakers)
 end
