f0d2bdc1daae060216fd2952c7a26fad59e7fbb5
Samuel Cornet <samuel@noukod.com>
2018-09-19 16:24:13 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -19,10 +19,14 @@ def assign_rooms(conference_speakers)
   thelist = Array.new()
   room_num = 1
   conference_speakers.each do |name|
-   messase_for_each_person_per_room = "Hello, #{name}! You'll be assigned to room #{room_num}!"
-   thelist << messase_for_each_person_per_room
+     messase_for_each_person_per_room = "Hello, #{name}! You'll be assigned to room #{room_num}!"
+     thelist << messase_for_each_person_per_room
    room_num += 1
   end
   return thelist
 end
-#
+
+def printer(conference_speakers)
+ batch_badge_creator(conference_speakers)
+ assign_rooms(conference_speakers)
+end
