67d8610e1d828b9b5c4682e7e3295f3d3e5fa8d2
Samuel Cornet <samuel@noukod.com>
2018-09-19 15:56:43 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,12 +1,14 @@
+conference_speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 def badge_maker(name)
   puts "Hello, my name is #{name}."
 end
 
 def batch_badge_creator(conference_speakers)
 
-  conference_speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+  
   conference_speakers.each do |name|
     message = badge_maker(name)
   end
    puts message
 end
+
