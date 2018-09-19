2ba18e00b767a2bfc84077fa72ec5ebdabaff123
Samuel Cornet <samuel@noukod.com>
2018-09-19 16:19:56 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -19,7 +19,7 @@ def assign_rooms(conference_speakers)
   thelist = Array.new()
   room_num = 1
   conference_speakers.each do |name|
-   messase_for_each_person_per_room = "Hello #{name}! You'll be assigned to room #{room_num}!"
+   messase_for_each_person_per_room = "Hello, #{name}! You'll be assigned to room #{room_num}!"
    thelist << messase_for_each_person_per_room
    room_num += 1
   end
